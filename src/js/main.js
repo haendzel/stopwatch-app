1be28d85a2397776ff16634067c2d8106b193c58
Filip Handzel <f.handzel@gmail.com>
2020-01-20 22:09:30 +0100

    sass, rwd, minify

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const btnMain = document.querySelector('button.main');
+const btnReset = document.querySelector('button.reset');
+const showTime = document.querySelector('div.time div');
+
+let interval;
+
+let clicks = 1;
+let time = 0;
+
+const timer = () => {
+    clicks++;
+    if (clicks % 2 == 0) {
+    btnMain.textContent = 'Pauza';
+    interval = setInterval(start, 10);
+    } else {
+    btnMain.textContent = 'Start';
+    clearInterval(interval)
+    }
+}
+
+const start = () => {
+    time++;
+    showTime.textContent = (time / 100).toFixed(2);
+}
+
+const reset = () => {
+    time = 0;
+    clicks = 1;
+    btnMain.textContent = 'Start';
+    showTime.textContent = '0.00';
+    clearInterval(interval)
+}
+
+window.addEventListener('keydown', timer);
+btnMain.addEventListener('click', timer);
+btnReset.addEventListener('click', reset);
+
+
+
+
+
